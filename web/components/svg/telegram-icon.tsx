e25fa4bb321c41bc4cd2d8708b82b40058affadf
Leon Eidelman <leoneidelman09@gmail.com>
2026-07-13 22:44:25 +0300

    fix(web): size telegram icon defaults

--- web/components/svg/telegram-icon.tsx
+++ web/components/svg/telegram-icon.tsx
@@ -4,6 +4,8 @@ export function TelegramIcon(props: React.SVGProps<SVGSVGElement>) {
       viewBox="0 0 24 24"
       xmlns="http://www.w3.org/2000/svg"
       aria-hidden="true"
+      width={24}
+      height={24}
       {...props}
     >
       <path
